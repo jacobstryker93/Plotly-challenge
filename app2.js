e8397cd8353c4aac295681f8d5cc7142cb076a48
jacobstryker93 <jacobstryker93@gmail.com>
2021-01-16 11:40:27 -0700

    Finished code for first trace

--- app2.js
+++ app2.js
@@ -1,27 +1,27 @@
 var select = d3.select("#selDataset");
 
 d3.json("samples.json").then((data) => {
-console.log(data);
+    console.log(data);
 
-var names = data.names;
+    var names = data.names;
 
-    names.forEach((sample)=> {
+    names.forEach((sample) => {
         selector
-         .append("option")
-         .text(sample)
-         .property("value", sample);
-});
-var initSample = names[0];
-buildCharts(initSample);
-metadata(initSample);
+            .append("option")
+            .text(sample)
+            .property("value", sample);
+    });
+    var initSample = names[0];
+    buildCharts(initSample);
+    metadata(initSample);
 });
 
 function metadata(sample) {
-    d3.json("sample.json").then((data) =>{
+    d3.json("sample.json").then((data) => {
         var metadata = data.metadata;
-       var filtered = metadata.filter((item) => item.id == sample); 
-       console.log(filtered[0]);
-        
+        var filtered = metadata.filter((item) => item.id == sample);
+        console.log(filtered[0]);
+
         var list = d3.select("#sample-metadata");
 
         list.html("");
@@ -36,19 +36,40 @@ function metadata(sample) {
 }
 
 function chartBuilder(sample) {
-d3.json("sample.json").then((data) => {
-    var samples = data.samples;
-    var sample_id = samples.filter(item => item.id == sample);
-    var result = sample_id[0];
+    d3.json("sample.json").then((data) => {
+        var samples = data.samples;
+        var sample_id = samples.filter(item => item.id == sample);
+        var result = sample_id[0];
 
-    var otu_ids = result.otu_ids;
-    var otu_labels = result.otu_labels;
-    var sample_values = result.sample_values;
-    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
 
-    var trace1 =  [
-    {
-        y
-    }]
-})
-}
+        var trace1 = [
+            {
+                y: yticks,
+                x: sample_values.slice(0, 10).reverse(),
+                text: otu_labels.slice(0, 10).reverse(),
+                mode: "markers",
+                marker: {
+                    color: 'blue',
+                }
+            }]
+    })
+};
+
+        var hBarLayout = {
+            title: "Top 10 Bacteria Found",
+            font:{
+                family: 'Arial'
+
+            },
+            margin: {t: 30, l: 150},
+            xaxis: {
+                title: "Sample Value",
+            },
+            yaxis: {
+                title: "OTU ID",
+            }
+        };
